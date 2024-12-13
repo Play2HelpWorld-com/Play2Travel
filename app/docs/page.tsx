aa1f100079a88342d022bea7e914e6edfaf921b6
Bahadur_mia <96464210+Bahadur777@users.noreply.github.com>
2024-12-14 00:55:38 +0600

    Change all of content

---
+++ app/docs/page.tsx
@@ -0,0 +1,12 @@
+import Docs from "@/components/Docs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Play2Travel | Play for Free Donation",
+  description: "This is built by Moyasi",
+  // other metadata
+};
+
+export default function DocsPage() {
+  return <Docs />;
+}
